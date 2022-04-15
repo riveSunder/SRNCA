2869be3b6ee07d3402bb5127c1b14136615d98b8
RiveSunder <RiveSunder@protonmail.com>
2022-04-15 14:18:49 -0600

    add setup.py and testing folder

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+
+setup(\
+        name="srnca", \
+        packages=["srnca", "testing"], \
+        version = "0.0 pre-alpha", \
+        description = "symbolic regression on neural cellular automata" \
+    )
+
+
