76169107619c9a57c0a0cc8c635a08b4d6516da7
RiveSunder <rivesunder@protonmail.com>
2023-05-28 18:20:41 -0600

    test commit summary: TOTAL                   131    121     8%

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(\
         name="srnca", \
         packages=["srnca", "testing"], \
-        version = "0.0 pre-alpha", \
+        version = "0.00", \
         description = "symbolic regression on neural cellular automata" \
     )
 
